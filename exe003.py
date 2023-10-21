eb76faa26b5f9860dfd682804bfc14fae1c99a5d
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-20 21:08:26 -0300

    Add files via upload

---
+++ exe003.py
@@ -0,0 +1,5 @@
+# Exercício Python #003 - Somando dois números
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma  = (numero1 + numero2)
+print("A soma entre {} e {} é {}".format(numero1, numero2, soma))

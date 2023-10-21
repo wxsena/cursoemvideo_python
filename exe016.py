579969ef6dcf81eff477d94223daca0359d1b50a
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-21 19:53:29 -0300

    Add files via upload

---
+++ exe016.py
@@ -0,0 +1,4 @@
+# Exercício Python #016 - Quebrando um número
+from math import trunc
+num = float(input("Digite um valor: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(num, trunc(num)))

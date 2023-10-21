c318f113456f383cbbf79db46902754877c92fdc
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-21 02:57:10 -0300

    Add files via upload

---
+++ exe005.py
@@ -0,0 +1,6 @@
+# Exercício Python #005 - Antecessor e Sucessor
+numero_inteiro = int(input("Digite um número: "))
+sucessor = numero_inteiro + 1
+antecessor = numero_inteiro - 1
+print("O sucessor de {} é {}.".format(numero_inteiro, sucessor))
+print("O antecessor de {} é {}.".format(numero_inteiro, antecessor))

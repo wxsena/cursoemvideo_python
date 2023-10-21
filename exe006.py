438f74d863896de926d4d65d37d45d893aecaa59
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-21 03:00:49 -0300

    Add files via upload

---
+++ exe006.py
@@ -0,0 +1,3 @@
+# Exercício Python #006 - Dobro, Triplo, Raiz Quadrada
+n = int(input("Digite um número: "))
+print("Dobro: {} \nTriplo: {} \nRaiz Quadrada: {}".format( n * 2 , n * 3 , n ** (1/2) ))

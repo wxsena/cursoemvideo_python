bc6ed2118833ccce17dad7f39b48265b71e5876f
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-21 19:34:15 -0300

    Add files via upload

---
+++ exe013.py
@@ -0,0 +1,4 @@
+# Exercício Python #013 - Reajuste Salarial
+slr = float(input("Parabéns pela promoção iminente. Por favor, forneça detalhes sobre sua remuneração atual: "))
+prom = slr * 1.15
+print("Seu novo salário será de R${}!".format(prom))

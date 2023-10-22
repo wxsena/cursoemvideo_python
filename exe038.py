cd0761bf303c40d1126f9c583f1b49d71a63131a
Matheus Sena <148593834+wxsena@users.noreply.github.com>
2023-10-21 23:20:56 -0300

    Add files via upload

---
+++ exe038.py
@@ -0,0 +1,9 @@
+# Exercício Python #038 - Comparando números
+número1 = int(input('Primeiro número: '))
+número2 = int(input('Segundo número: '))
+if número1 > número2:
+    print('O PRIMEIRO valor é maior')
+elif número2 > número1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são IGUAIS')
